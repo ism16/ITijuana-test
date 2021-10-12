00497414d947babdc070ce75bb7e391940811891
ism16 <30345278+ism16@users.noreply.github.com>
2021-10-12 04:44:11 -0700

    Add: math.py file for math questions, and test_challenges.py for holding unittests.

---
+++ challenges/math.py
@@ -0,0 +1,29 @@
+from typing import List
+
+
+def question_1(x: int, y: int) -> List[int]:
+    '''
+    Write a function that takes two integers (x and y) and returns a list of numbers between x and y that are divisible by 5 but not by 7.
+
+    Answer = Bewteen x and y is a key factor, thus, this is a not inclusive range, x and y cannot be in list.
+    '''
+    return
+
+def question_2(x: int, y: int) -> List[int]:
+    '''
+    Write a function that takes two numbers (x and y) as input and returns the value to the following function:
+    
+    Answer = There is not funtcion in the pdf. I assume it refers to function 3, for this, instead of taking two arguments, 
+    I will make use of python built-in inputs to built a CLI and then call function 3.
+    '''
+    return
+
+def question_3(x: int, y: int) -> List[int]:
+    '''
+    Write a function that takes two integers (x and b) as inputs and returns a string that represents the number x in base b
+    Example 1: if x=5 and b=2 then the function will return "101"
+    Example 2: if x=5 and b=3 then the function will return "12"
+
+    Answer =
+    '''
+    return
---
+++ challenges/tests/test_challenges.py
@@ -0,0 +1,16 @@
+import pytest
+import sys
+import os
+
+sys.path.append(os.path.split(os.path.dirname(os.path.realpath( __file__ )))[0])
+
+from math import *
+
+def test_question_1():
+    return
+
+def test_question_2():
+    return
+
+def test_question_3():
+    return
